1345bff36495588910a1ef938ab2056a1611b2a8
dnwdark <Artec40@gmail.com>
2020-07-20 20:41:33 +0500

    Урок 14. css-модули

--- Projects/01-first-project/react-kabzda-1/src/components/Header.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import s from './Header.module.css'
 
 const Header = () => {
-    return <header className={'header'}>
+    return <header className={s.header}>
         <img src={'https://seeklogo.com/images/C/company-name-logo-C74A7D6F5A-seeklogo.com.png'}/>
     </header>
 }
--- Projects/01-first-project/react-kabzda-1/src/components/Navbar.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Navbar.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
+import s from './Navbar.module.css'
+
+//let s ={
+//    'nav' : 'Navbar_nav_30u9Q',
+//    'item' : 'Navbar_item_3qaF3'
+//    'active': "Navbar_active_...'
+//}
+console.log(s);
+let c1 = "item";
+let c2 = "active";
+let classes = c1+ "" +c2; //item active
+let classesNew = '${s.item} ${s.active}';
 
 const Navbar = () => {
-    return <nav className={'nav'}>
-        <div>
+    return <nav className={s.nav}>
+        <div className={s.item}>
             <a>Profile</a>
         </div>
-        <div>
+        <div className={`${s.item} ${s.active}`}>
             <a>Messages</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a>News</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a>Music</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a>Settings</a>
         </div>
     </nav>
