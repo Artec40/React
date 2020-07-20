0289e2a6657101c320038f29c8295be204870aa7
dnwdark <Artec40@gmail.com>
2020-07-20 18:19:03 +0500

    Урок 13

---
+++ Projects/01-first-project/react-kabzda-1/src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Profile = () => {
+    return <div className={'content'}>
+        <div>
+            <img src={'https://fullhdwall.com/wp-content/uploads/2018/07/Ocean-Image.jpeg'}/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile
