b760eee73762551f3df53a8c92fefe7b3ef35488
dnwdark <Artec40@gmail.com>
2020-08-02 10:28:49 +0500

    Урок 28. Выносим данные в index.js

--- Projects/01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,16 +2,10 @@ import React from 'react'
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
-        {id: 1, message: 'Hi, how are you?', likesCount: 12},
-        {id: 2, message: 'It\'s my first post', likesCount: 11},
-        {id: 3, message: 'Blabla', likesCount: 11},
-        {id: 4, message: 'Dada', likesCount: 11},
-    ]
-
-    let postsElements = posts.map(p => <Post message={p.message} likeCount={p.likesCount}/>)
+    let postsElements =
+        props.posts.map(p => <Post message={p.message} likeCount={p.likesCount}/>)
 
     return (
         <div className={s.postsBlock}>
