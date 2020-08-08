cb0260b7a083664f55e1744603bcb5b40ace04c4
dnwdark <Artec40@gmail.com>
2020-08-08 11:05:24 +0500

    Урок 45. connect, mapStateToProps, map DispatchToProps.

--- Projects/01-first-project/react-kabzda-1/src/components/Dialogs/DialogsContainer.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Dialogs/DialogsContainer.jsx
@@ -1,31 +1,24 @@
 import React from 'react'
-import s from './Dialogs.module.css'
-import Message from './Message/Message'
-import DialogItem from './DialogItem/DialogItem'
 import { updateNewMessageBodyCreator, sendMessageCreator } from '../../redux/dialogs-reducer'
 import Dialogs from './Dialogs'
-import StoreContext from '../../StoreContext'
+import { connect } from 'react-redux'
 
-const DialogsContainer = (props) => {
-
-    return <StoreContext.Consumer>
-        {(store) => {
-            let state = store.getState().dialogsPage
-
-            let onSendMessageClick = () => {
-                store.dispatch(sendMessageCreator())
-            }
-
-            let NewMessageChange = (body) => {
-                store.dispatch(updateNewMessageBodyCreator(body))
-            }
-
-            return <Dialogs updateNewMessageBody={NewMessageChange}
-                            sendMessage={onSendMessageClick}
-                            dialogsPage={state}/>
-        }
-        }
-    </StoreContext.Consumer>
+let mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
 }
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewMessageBody: (body) => {
+            dispatch(updateNewMessageBodyCreator(body))
+        },
+        sendMessage: () => {
+            dispatch(sendMessageCreator())
+        }
+    }
+}
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs)
 
 export default DialogsContainer
--- Projects/01-first-project/react-kabzda-1/src/components/Navbar/NavbarContainer.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Navbar/NavbarContainer.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
-import StoreContext from '../../StoreContext'
+import { connect } from 'react-redux'
 
-const NavbarContainer = (props) => {
-
-    return (<StoreContext.Consumer>
-            {(store) => (
-                <Navbar friends={store.getState().sidebar.friends}/>
-            )
-            }
-        </StoreContext.Consumer>
-    )
+const mapStateToProps = (state) => {
+    return {
+        friends: state.sidebar.friends
+    }
 }
 
+const mapDispatchToProps = (dispatch) => {
+    return {}
+}
+
+const NavbarContainer = connect(mapStateToProps, mapDispatchToProps)(Navbar)
 export default NavbarContainer
