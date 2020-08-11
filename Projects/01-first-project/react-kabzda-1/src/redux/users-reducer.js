5d918ae74be659ab6639cfafbde7e82aae028e74
dnwdark <Artec40@gmail.com>
2020-08-12 00:35:20 +0500

    Урок 56. Презентационная и Контейнерная компоненты.

--- Projects/01-first-project/react-kabzda-1/src/redux/users-reducer.js
+++ Projects/01-first-project/react-kabzda-1/src/redux/users-reducer.js
@@ -7,7 +7,7 @@ const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 let initialState = {
     users: [],
     pageSize: 5,
-    totalUsersCount: 5,
+    totalUsersCount: 50,
     currentPage: 1
 }
 
