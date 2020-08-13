f89f862e0b7141557e8e0fd2893e231d606d5c29
dnwdark <Artec40@gmail.com>
2020-08-13 22:45:51 +0500

    Урок 63. DAL, axios.create.

---
+++ Projects/01-first-project/react-kabzda-1/src/api/api.js
@@ -0,0 +1,27 @@
+import * as axios from 'axios/index'
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': 'c2569082-1356-497c-8ca3-54489a49ed1b'
+    }
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    }
+}
+
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+        .then(response => {
+            return response.data
+        })
+}
