a9ae92cfb56e417d1327db3f1a7d73156b337afb
dnwdark <Artec40@gmail.com>
2020-08-13 20:47:53 +0500

    Урок 62. follow-unfollow api.

--- Projects/01-first-project/react-kabzda-1/src/components/Users/Users.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import userPhoto from '../../assets/images/user.png'
 import styles from './users.module.css'
 import { NavLink } from 'react-router-dom'
+import * as axios from 'axios/index'
 
 let Users = (props) => {
 
@@ -32,10 +33,34 @@ let Users = (props) => {
                 <div>
                     {u.followed
                         ? <button onClick={() => {
-                            props.unfollow(u.id)
+
+                            axios.delete(`https://social-network.samuraijs.com/api/1.0/unfollow/${u.id}`, {
+                                withCredentials: true,
+                                headers: {
+                                    'API-KEY': 'c2569082-1356-497c-8ca3-54489a49ed1b'
+                                }
+                            })
+                                .then(response => {
+                                    if (response.data.resultCode == 0) {
+                                        props.unfollow(u.id)
+                                    }
+                                })
+
                         }}>Unfollow</button>
                         : <button onClick={() => {
-                            props.follow(u.id)
+
+                            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                withCredentials: true,
+                                headers: {
+                                    'API-KEY': 'c2569082-1356-497c-8ca3-54489a49ed1b'
+                                }
+                            })
+                                .then(response => {
+                                    if (response.data.resultCode == 0) {
+                                        props.follow(u.id)
+                                    }
+                                })
+
                         }}>Follow</button>}
                 </div>
                 </span>
