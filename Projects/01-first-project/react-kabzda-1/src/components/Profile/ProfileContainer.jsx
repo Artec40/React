8eeb4d9a2a65c88793995b8af90e8c4077505d1c
dnwdark <Artec40@gmail.com>
2020-08-15 18:45:24 +0500

    Урок 73. http put, local state.

--- Projects/01-first-project/react-kabzda-1/src/components/Profile/ProfileContainer.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Profile/ProfileContainer.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Profile from './Profile'
-import * as axios from 'axios/index'
 import { connect } from 'react-redux'
-import { getUserProfile } from '../../redux/profile-reducer'
-import { Redirect, withRouter } from 'react-router-dom'
-import { withAuthRedirect } from '../../hoc/withAuthRedirect'
+import { withRouter } from 'react-router-dom'
 import { compose } from 'redux'
+import { getUserProfile, getStatus, updateStatus } from '../../redux/profile-reducer'
 
 class ProfileContainer extends React.Component {
 
@@ -15,20 +13,23 @@ class ProfileContainer extends React.Component {
             userId = 2
         }
         this.props.getUserProfile(userId)
+        this.props.getStatus(userId)
     }
 
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile}/>
+            <Profile {...this.props} profile={this.props.profile} status={this.props.status}
+                     updateStatus={this.props.updateStatus}/>
         )
     }
 }
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile
+    profile: state.profilePage.profile,
+    status: state.profilePage.status
 })
 
 export default compose(
-    connect(mapStateToProps, {getUserProfile}),
+    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
     withRouter,
 )(ProfileContainer)
--- Projects/01-first-project/react-kabzda-1/src/redux/profile-reducer.js
+++ Projects/01-first-project/react-kabzda-1/src/redux/profile-reducer.js
@@ -1,8 +1,9 @@
-import { usersAPI } from '../api/api'
+import { usersAPI, profileAPI } from '../api/api'
 
 const ADD_POST = 'ADD-POST'
 const UPDATE_TWO_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
+const SET_STATUS = 'SET_STATUS'
 
 let initialState = {
     posts: [
@@ -12,7 +13,8 @@ let initialState = {
         {id: 4, message: 'Dada', likesCount: 11},
     ],
     newPostText: 'it-kamasutra.com',
-    profile: null
+    profile: null,
+    status: ''
 }
 
 const profileReducer = (state = initialState, action) => {
@@ -37,16 +39,35 @@ const profileReducer = (state = initialState, action) => {
             }
         }
 
+        case SET_STATUS: {
+            return {
+                ...state, status: action.status
+            }
+        }
+
         default:
             return state
     }
 }
 
 export const addPostActionCreator = () => ({type: ADD_POST})
-export const updateNewPostTextActionCreator = (text) =>
-    ({type: UPDATE_TWO_POST_TEXT, newText: text})
-export const setUserProfile = (profile) =>
-    ({type: SET_USER_PROFILE, profile})
+export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_TWO_POST_TEXT, newText: text})
+export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
+export const setStatus = (status) => ({type: SET_STATUS, status})
+
+export const getStatus = (userId) => (dispatch) => {
+    profileAPI.getStatus(userId).then(response => {
+        dispatch(setStatus(response.data))
+    })
+}
+
+export const updateStatus = (status) => (dispatch) => {
+    profileAPI.updateStatus(status).then(response => {
+        if (response.data.resultCode === 0) {
+            dispatch(setStatus(status))
+        }
+    })
+}
 
 export const getUserProfile = (userId) => (dispatch) => {
     usersAPI.getProfile(userId).then(response => {
