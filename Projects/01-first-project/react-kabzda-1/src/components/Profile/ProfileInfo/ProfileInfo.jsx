8add8856df6d3c55bec10a75656d27d647d8aa6f
dnwdark <Artec40@gmail.com>
2020-08-15 16:08:31 +0500

    Урок 71. setState, local state.

--- Projects/01-first-project/react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import s from './ProfileInfo.module.css'
 import Preloader from '../../common/Preloader/Preloader'
-
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -10,12 +10,12 @@ const ProfileInfo = (props) => {
 
     return (
         <div>
-            <div>
+{/*            <div>
                 <img src={'https://fullhdwall.com/wp-content/uploads/2018/07/Ocean-Image.jpeg'}/>
-            </div>
+            </div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                ava + description
+                <ProfileStatus status={'Hello my friends'}/>
             </div>
         </div>
     )
