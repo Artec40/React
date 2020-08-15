14177c79483e3e82ef46776a3efa70e73f7dffdc
dnwdark <Artec40@gmail.com>
2020-08-16 01:04:55 +0500

    Урок 76. redux-form базовые вещи.

--- Projects/01-first-project/react-kabzda-1/src/redux/profile-reducer.js
+++ Projects/01-first-project/react-kabzda-1/src/redux/profile-reducer.js
@@ -1,7 +1,6 @@
 import { usersAPI, profileAPI } from '../api/api'
 
 const ADD_POST = 'ADD-POST'
-const UPDATE_TWO_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
 const SET_STATUS = 'SET_STATUS'
 
@@ -12,7 +11,6 @@ let initialState = {
         {id: 3, message: 'Blabla', likesCount: 11},
         {id: 4, message: 'Dada', likesCount: 11},
     ],
-    newPostText: 'it-kamasutra.com',
     profile: null,
     status: ''
 }
@@ -20,20 +18,13 @@ let initialState = {
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST: {
-            let newPost = {id: 5, message: state.newPostText, likesCount: 0}
+            let newPost = {id: 5, message: action.newPostText, likesCount: 0}
             return {
                 ...state,
                 posts: [...state.posts, newPost],
-                newPostText: ''
             }
         }
-        case UPDATE_TWO_POST_TEXT: {
-            return {
-                ...state,
-                newPostText: action.newText
-            }
-        }
-        case SET_USER_PROFILE: {
+             case SET_USER_PROFILE: {
             return {
                 ...state, profile: action.profile
             }
@@ -50,8 +41,7 @@ const profileReducer = (state = initialState, action) => {
     }
 }
 
-export const addPostActionCreator = () => ({type: ADD_POST})
-export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_TWO_POST_TEXT, newText: text})
+export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText})
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
 export const setStatus = (status) => ({type: SET_STATUS, status})
 
