1d1b86ac82c0989068863e6f6f8797058e89f90d
dnwdark <Artec40@gmail.com>
2020-08-15 14:51:49 +0500

    Урок 70. Функция compose.

--- Projects/01-first-project/react-kabzda-1/src/components/Users/UsersContainer.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Users/UsersContainer.jsx
@@ -10,6 +10,7 @@ import {
 import Users from './Users'
 import Preloader from '../common/Preloader/Preloader'
 import { withAuthRedirect } from '../../hoc/withAuthRedirect'
+import { compose } from 'redux'
 
 
 
@@ -74,6 +75,7 @@ let mapStateToProps = (state) => {
     }
 }*/
 
-export default withAuthRedirect(connect(mapStateToProps, {
-    follow, unfollow, setCurrentPage,
-    toggleFollowingProgress, getUsers})(UsersContainer))
+export default compose(
+    withAuthRedirect,
+    connect(mapStateToProps, {follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers})
+)(UsersContainer)
