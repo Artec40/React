741451b09cf2cb78020fc59a72aa21fca6170061
dnwdark <Artec40@gmail.com>
2020-08-17 21:40:29 +0500

    Урок 80. Инициализация приложения.

--- Projects/01-first-project/react-kabzda-1/src/App.js
+++ Projects/01-first-project/react-kabzda-1/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import HeaderContainer from './components/Header/HeaderContainer'
 import ProfileContainer from './components/Profile/ProfileContainer'
-import { Route } from 'react-router-dom'
+import { Route, withRouter } from 'react-router-dom'
 import News from './components/News/News'
 import Music from './components/Music/Music'
 import Settings from './components/Settings/Settings'
@@ -11,26 +11,53 @@ import DialogsContainer from './components/Dialogs/DialogsContainer'
 import NavbarContainer from './components/Navbar/NavbarContainer'
 import UsersContainer from './components/Users/UsersContainer'
 import LoginPage from './Login/Login'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { initializeApp } from '../src/redux/app-reducer'
+import Preloader from '../src/components/common/Preloader/Preloader'
 
-const App = (props) => {
-    return (
-        <div className={'app-wrapper'}>
-            <HeaderContainer/>
-            <NavbarContainer/>
-            <div className={'app-wrapper-content'}>
-                <Route path={'/dialogs'}
-                       render={() => <DialogsContainer/>}/>
-                <Route path={'/profile/:userId?'}
-                       render={() => <ProfileContainer/>}/>
-                <Route path={'/news'} render={() => <News/>}/>
-                <Route path={'/users'} render={() => <UsersContainer/>}/>
-                                <Route path={'/music'} render={() => <Music/>}/>
-                <Route path={'/settings'} render={() => <Settings/>}/>
-                <Route path={'/friends'} render={() => <Friends/>}/>
-                <Route path={'/login'} render={() => <LoginPage/>}/>
+class App extends Component {
+    componentDidMount() {
+        this.props.initializeApp()
+    }
+
+    render() {
+        if (!this.props.initialized) {
+            return <Preloader/>
+        }
+        return (
+            <div className={'app-wrapper'}>
+                <HeaderContainer/>
+                <NavbarContainer/>
+                <div className={'app-wrapper-content'}>
+                    <Route path={'/dialogs'}
+                           render={() => <DialogsContainer/>}/>
+
+                    <Route path={'/profile/:userId?'}
+                           render={() => <ProfileContainer/>}/>
+
+                    <Route path={'/news'} render={() => <News/>}/>
+
+                    <Route path={'/users'}
+                           render={() => <UsersContainer/>}/>
+
+                    <Route path={'/music'} render={() => <Music/>}/>
+                    <Route path={'/settings'} render={() => <Settings/>}/>
+                    <Route path={'/friends'} render={() => <Friends/>}/>
+
+                    <Route path={'/login'}
+                           render={() => <LoginPage/>}/>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default App
+const mapStateToProps = (state) => ({
+    initialized: state.app.initialized
+})
+
+export default compose(
+    withRouter,
+    connect(mapStateToProps, {initializeApp}))
+(App)
--- Projects/01-first-project/react-kabzda-1/src/components/Users/UsersContainer.jsx
+++ Projects/01-first-project/react-kabzda-1/src/components/Users/UsersContainer.jsx
@@ -76,6 +76,5 @@ let mapStateToProps = (state) => {
 }*/
 
 export default compose(
-    withAuthRedirect,
     connect(mapStateToProps, {follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers})
 )(UsersContainer)
